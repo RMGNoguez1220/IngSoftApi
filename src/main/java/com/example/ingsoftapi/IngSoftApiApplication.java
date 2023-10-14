19afe467649078368dc902649fa297b401b7cb3f
RMGNoguez1220 <19030537@itcelaya.edu.mx>
2023-10-13 23:09:31 -0600

    Versión Inicial

---
+++ src/main/java/com/example/ingsoftapi/IngSoftApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingsoftapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngSoftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngSoftApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ingsoftapi/IngSoftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingsoftapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngSoftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
