a62e58ae0c38b8231784045cbac12b7cdc37091d
AlejandroMendozaP <alemenpa2002@gmail.com>
2023-10-14 20:28:30 -0600

    Create CategoriaR.java

---
+++ src/main/java/com/example/ingsoftapi/domain/CategoriaR.java
@@ -0,0 +1,47 @@
+package com.example.ingsoftapi.domain;
+
+import lombok.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CategoriaR extends R {
+    private Categoria categoria;
+    private List<Categoria> listCategoria;
+    HashMap<String,Object> data = new HashMap<>();
+    public CategoriaR(Categoria categoria, String message, int status, boolean flag){
+        super(flag, message, status);
+        this.categoria=categoria;
+
+    }
+    public CategoriaR(String message,int status,boolean flag){
+        super(flag, message, status);
+
+    }
+    public CategoriaR(List<Categoria> listCategoria,String message, int status, boolean flag){
+        super(flag, message, status);
+        this.listCategoria=listCategoria;
+
+    }
+    public Object response() {
+        data.put("Flag", isFlag());
+        data.put("Message", getMessage());
+        data.put("Status", getStatusCode());
+        data.put("Data", this.categoria);
+        return data;
+    }
+
+    public Object response2() {
+        data.put("Flag", isFlag());
+        data.put("Message", getMessage());
+        data.put("Status", getStatusCode());
+        data.put("Data", this.listCategoria);
+        return data;
+    }
+}
+
