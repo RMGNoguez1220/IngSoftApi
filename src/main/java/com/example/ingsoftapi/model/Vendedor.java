9c13cda841209805b1c2bb9c05791bf3cf738be4
Lluvivazquez001 <vlluvia494@gmail.com>
2023-10-16 12:29:17 -0600

    Creacion de model vendedor

---
+++ src/main/java/com/example/ingsoftapi/model/Vendedor.java
@@ -0,0 +1,19 @@
+package com.example.ingsoftapi.model;
+import jakarta.persistence.*;
+import lombok.*; //es empleada para usar todas las que conlleva o puede tener como GET, SETTER, NoArgsConstructor, ETC
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table
+public class Vendedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_vendedor;
+    private String nombre_tienda;
+    private String descripcion_tienda;
+    private String categorias;
+}
