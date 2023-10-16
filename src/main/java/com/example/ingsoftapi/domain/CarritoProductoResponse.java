eff1bf6d2e2d0e6c233592423a953247c43ac477
AlejandroMendozaP <alemenpa2002@gmail.com>
2023-10-15 22:11:22 -0600

    Creacion de la clase CarritoProductoResponse

---
+++ src/main/java/com/example/ingsoftapi/domain/CarritoProductoResponse.java
@@ -0,0 +1,49 @@
+package com.example.ingsoftapi.domain;
+
+import com.example.ingsoftapi.model.CarritoProducto;
+import lombok.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class CarritoProductoResponse extends SuperResponse{
+    private CarritoProducto carritoproducto;
+    private List<CarritoProducto> carritoproductoList;
+    private HashMap<String, Object> data = new HashMap<>();
+
+    public CarritoProductoResponse(final List<CarritoProducto> vendedorList, final String message, final int status, final boolean flag) {
+        super(flag, message, status);
+        this.carritoproductoList = vendedorList;
+    }
+
+    public CarritoProductoResponse(String message, int status, boolean flag) {
+        super(flag, message, status);
+    }
+
+    public CarritoProductoResponse(CarritoProducto carritoproducto, String message, int status, boolean flag) {
+        super (flag, message, status);
+        this.carritoproducto = carritoproducto;
+    }
+
+    public Object responseAll() {
+        data.put("Flag", isFlag());
+        data.put("Message", getMessage());
+        data.put("Status", getStatusCode());
+        data.put("Data", this.carritoproductoList);
+        return data;
+    }
+
+    public Object responseInsert() {
+        data.put("Flag", isFlag());
+        data.put("Message", getMessage());
+        data.put("Status", getStatusCode());
+        data.put("Data", this.carritoproducto);
+        return data;
+    }
+}
