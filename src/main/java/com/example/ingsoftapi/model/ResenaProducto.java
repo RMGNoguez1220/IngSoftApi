0091cfce63e9d09732cede4bd29cf77f366fe3a8
Lluvivazquez001 <vlluvia494@gmail.com>
2023-10-16 19:13:58 -0600

    Creacion de model de resena producto

---
+++ src/main/java/com/example/ingsoftapi/model/ResenaProducto.java
@@ -0,0 +1,22 @@
+package com.example.ingsoftapi.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table
+public class ResenaProducto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_resena;//llama primaria
+    private long id_usuario; //llave foranea
+    private long id_producto; //llave foranea
+    private int cantidad;
+    private String comentario;
+
+}
