458f63f63971affeea11d76220fc539618249e08
Lluvivazquez001 <vlluvia494@gmail.com>
2023-10-16 19:08:14 -0600

    Creacion de model carrito producto

---
+++ src/main/java/com/example/ingsoftapi/model/CarritoProducto.java
@@ -0,0 +1,19 @@
+package com.example.ingsoftapi.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table
+public class CarritoProducto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_carrito;//llama primaria
+    private long id_producto;//llave foranea
+    private int cantidad;
+}
